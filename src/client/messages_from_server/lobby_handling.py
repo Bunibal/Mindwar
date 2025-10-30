679094d7ec4e904eb85280a39fe50963a8a2e558
Sebastian <sebastian.bittner99@gmail.com>
2025-10-30 20:42:29 +0100

    fixes

--- src/client/messages_from_server/lobby_handling.py
+++ src/client/messages_from_server/lobby_handling.py
@@ -3,9 +3,9 @@ def lobby_info_received(message: str, ui_manager):
     ui_manager.lobby_info_received(message)
 
 
-def send_lobby_list(connection, time_received, lobbies: list[str]):
+def send_lobby_list(lobbies: list[str], ui_manager):
     """Send the list of lobbies from server to client. (Meaning the client will be receiving)"""
     print(f"Received lobby list from server: {lobbies}")
-    
+
     ui_manager.lobby_list_received(lobbies)
     # Update local lobby list UI
--- src/common/messages_from_server/message_types.py
+++ src/common/messages_from_server/message_types.py
@@ -1,9 +1,10 @@
 from enum import Enum, auto
-from src.client.messages_from_server.lobbies import *
+from src.client.messages_from_server.lobby_handling import *
 
 class MessageType(Enum):
-    LOBBY_INFO = lobby_info_received
-    LOBBY_LIST = send_lobby_list
+    LOBBY_INFO = (lobby_info_received,) # Yes we need a singleton tuple here every time
+    LOBBY_LIST = (send_lobby_list,) # Enum works very strange when the value is a function
+
 
 
 
